4e52559cfa067c7eded4fb93d84d54636bec8847
Grow Search <marcelocx1904@gmail.com>
2022-10-11 16:43:33 -0300

    SLIDE

--- assets/javaScript/main.js
+++ assets/javaScript/main.js
@@ -1,10 +1,18 @@
 
 import servicesModal from "./modules/servicos.js";
-import projetosSwiper from "./modules/projetosSwip.js";
+//import projetosSwiper from "./modules/projetosSwip.js";
 import escrevendoTela from "./modules/maqEscrever.js";
 import fraseAnima from "./modules/frase.js";
+import slideNav from "./modules/slide.js";
+import debounce from "./modules/debounce.js"
 
 fraseAnima();
 escrevendoTela();
-projetosSwiper();
+//projetosSwiper();
 servicesModal();
+debounce();
+
+const slide = new slideNav('.slide' , '.wrapper');
+slide.init();
+slide.addArrow('.prev' , '.next');
+slide.addControl('.custom-controls');
