29602d135c85838884e467ed81f68a62402676c7
Grow Search <marcelocx1904@gmail.com>
2022-10-25 18:08:07 -0300

    mudando cor e modais

--- assets/javaScript/main.js
+++ assets/javaScript/main.js
@@ -4,13 +4,12 @@ import escrevendoTela from "./modules/maqEscrever.js";
 import fraseAnima from "./modules/frase.js";
 import slideNav from "./modules/slide.js";
 import debounce from "./modules/debounce.js"
-import formularioModal from "./modules/formModal.js"
 
 fraseAnima();
 escrevendoTela();
 servicesModal();
 debounce();
-formularioModal();
+
 
 const slide = new slideNav('.slide' , '.wrapper');
 slide.init();
--- assets/javaScript/modules/servicos.js
+++ assets/javaScript/modules/servicos.js
@@ -1,9 +1,9 @@
 
 // ========== SERVICES MODAL =========== //
 export default function servicesModal() {
-const modalViews = document.querySelectorAll('.services__modal'),
-modalBtns = document.querySelectorAll('.services__button'),
-modalCloses = document.querySelectorAll('.services__modal-close');
+const modalViews = document.querySelectorAll('.services__modal , .modal__form'),
+modalBtns = document.querySelectorAll('.services__button , .buttonContact'),
+modalCloses = document.querySelectorAll('.services__modal-close , .modal__form-close');
 
 let modal = function(modalClick) {
     modalViews[modalClick].classList.add('active-modal');
