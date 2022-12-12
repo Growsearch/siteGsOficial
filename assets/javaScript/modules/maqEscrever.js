3f37da358998be45b7ef17b8fcc4bedb51cd0bc7
Grow Search <marcelocx1904@gmail.com>
2022-12-12 18:32:55 -0300

    adicionando funcoes de escrever e apagar
    modifivando um pouco do design do site hoje

--- assets/javaScript/modules/maqEscrever.js
+++ assets/javaScript/modules/maqEscrever.js
@@ -4,7 +4,7 @@
 
 export default function escrevendoTela() {
 
-    function maquinaEscrev(elemento) {
+    /* function maquinaEscrev(elemento) {
         const textoProd = elemento.innerHTML.split('');
         elemento.innerHTML = '';
         textoProd.forEach ((letra , i ) => {
@@ -14,6 +14,51 @@ export default function escrevendoTela() {
     
     const prodSpeak = document.querySelector('.tituloPrincipal');
     maquinaEscrev(prodSpeak);
-    
+    */
+
+var div = document.querySelector('.tituloPrincipal');
+var textos = ['Ajudamos você a posicionar o seu negócio na Internet', 'Construimos o melhor site para você', 'Torne-se uma autoridade no seu ramo de trabalho', 'Converse conosco via chat'];
+
+function escrever(str, done) {
+    var char = str.split('').reverse();
+    var typer = setInterval(function() {
+        if (!char.length) {
+            clearInterval(typer);
+            return setTimeout(done, 600); // só para esperar um bocadinho
+        }
+        var next = char.pop();
+        div.innerHTML += next;
+    }, 100);
+}
+
+function limpar(done) {
+    var char = div.innerHTML;
+    var nr = char.length;
+    var typer = setInterval(function() {
+        if (nr-- == 0) {
+            clearInterval(typer);
+            return done();
+        }
+        div.innerHTML = char.slice(0, nr);
+    }, 100);
+}
+
+function rodape(conteudos, el) {
+    var atual = -1;
+    function prox(cb){
+        if (atual < conteudos.length - 1) atual++;
+        else atual = 0;
+        var str = conteudos[atual];
+        escrever(str, function(){
+            limpar(prox);
+        });
     }
+    prox(prox);
+}
+rodape(textos);
+
+}
+
+    
+
     
