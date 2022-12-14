5d911b5bad23645837e3f9c5727aa861766787ef
Grow Search <marcelocx1904@gmail.com>
2022-12-14 18:49:06 -0300

    melhoria em design do site e mobile

--- assets/javaScript/main.js
+++ assets/javaScript/main.js
@@ -1,7 +1,7 @@
 
 import servicesModal from "./modules/servicos.js";
 import escrevendoTela from "./modules/maqEscrever.js";
-import fraseAnima from "./modules/frase.js";
+//import fraseAnima from "./modules/frase.js";
 import slideNav from "./modules/slide.js";
 import debounce from "./modules/debounce.js"
 import animaCard from "./modules/animaCard.js"
@@ -9,7 +9,7 @@ import contatenos from"./modules/contateModal.js";
 import darkMode from"./modules/darkMode.js";
 import menuMobile from"./modules/menuMobile.js";
 
-fraseAnima();
+//fraseAnima();
 escrevendoTela();
 servicesModal();
 debounce();
